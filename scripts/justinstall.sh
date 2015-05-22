cccdc24214e217ff17e2b9ea8f43a722055c6139
Ravi Prakash <raviprak@altiscale.com>
2015-05-21 19:07:11 -0700

    justinstall now working on my laptop

--- scripts/justinstall.sh
+++ scripts/justinstall.sh
@@ -1,25 +1,38 @@
 #!/bin/sh -ex
-# deal with the fuse artifacts to create a tarball
-ALTISCALE_RELEASE=${ALTISCALE_RELEASE:-0.1.0}
 
-tar -C ${WORKSPACE}/hadoop-common/hadoop-hdfs-project/hadoop-hdfs/target/native/main/native/fuse-dfs -cvzf ${WORKSPACE}/hadoop-common/hadoop-dist/target/fuse-${ARTIFACT_VERSION}.tar.gz fuse_dfs
+# Although zookeeper's ant build does have an 'rpm' target we roll our own
+# because the destination directory for us is different (/opt/zookeeper-x.y.z)
 
-# convert each tarball into an RPM
-DEST_ROOT=${INSTALL_DIR}/opt/fuse-${ARTIFACT_VERSION}
-mkdir --mode=0755 -p ${DEST_ROOT}
-cd ${DEST_ROOT}
-tar -xvzpf ${WORKSPACE}/hadoop-common/hadoop-dist/target/fuse-${ARTIFACT_VERSION}.tar.gz
-chmod 500 ${DEST_ROOT}/fuse_dfs
+export WORKSPACE=${WORKSPACE:-.}
+export ALTISCALE_RELEASE=${ALTISCALE_RELEASE:-4.0.0}
+export DATE_STRING=`date +"%Y%m%d%H%M"`
+export INSTALL_DIR=${WORKSPACE}/install-${DATE_STRING}
+export DESCRIPTION=${DESCRIPTION:-"Altiscale Zookeeper rpm"}
 
-cd ${RPM_DIR}
+if [ -z "$ARTIFACT_VERSION" ]; then
+  echo "Please define the ARTIFACT_VERSION. This is usually the version of the project you're trying to build"
+  exit 1;
+fi
 
-export RPM_NAME=vcc-fuse-${ARTIFACT_VERSION}
+
+# Create the base directory where fpm will be run
+rm -rf ${INSTALL_DIR}/opt
+mkdir --mode=0755 -p ${INSTALL_DIR}/opt
+
+# Copy the tar.gz file (we expect zookeeper-x.y.z.tar.gz)
+cp build/zookeeper-?.?.?.tar.gz ${INSTALL_DIR}/opt
+
+#Untar the tarball
+cd ${INSTALL_DIR}/opt
+tar xvf zookeeper-?.?.?.tar.gz
+
+#Run fpm to create the rpm
+export RPM_NAME=alti-zookeeper-${ARTIFACT_VERSION}
 fpm --verbose \
---maintainer ops@verticloud.com \
---vendor VertiCloud \
+--maintainer ops@altiscale.com \
+--vendor Altiscale \
 --provides ${RPM_NAME} \
 --description "${DESCRIPTION}" \
---replaces vcc-fuse \
 -s dir \
 -t rpm \
 -n ${RPM_NAME} \
@@ -29,58 +42,3 @@ fpm --verbose \
 --rpm-group root \
 -C ${INSTALL_DIR} \
 opt
-
-#clear the install dir and re-use
-rm -rf ${INSTALL_DIR}
-mkdir -p --mode 0755 ${INSTALL_DIR}
-
-OPT_DIR=${INSTALL_DIR}/opt
-mkdir --mode=0755 -p ${OPT_DIR}
-cd ${OPT_DIR}
-
-tar -xvzpf ${WORKSPACE}/hadoop-common/hadoop-dist/target/hadoop-${ARTIFACT_VERSION}.tar.gz
-chmod 755 ${OPT_DIR}/hadoop-${ARTIFACT_VERSION}
-# https://verticloud.atlassian.net/browse/OPS-731
-# create /etc/hadoop, in a future version of the build we may move the config there directly
-ETC_DIR=${INSTALL_DIR}/etc/hadoop-${ARTIFACT_VERSION}
-mkdir --mode=0755 -p ${ETC_DIR}
-# move the config directory to /etc
-cp -rp ${OPT_DIR}/hadoop-${ARTIFACT_VERSION}/etc/hadoop/* $ETC_DIR
-mv ${OPT_DIR}/hadoop-${ARTIFACT_VERSION}/etc/hadoop ${OPT_DIR}/hadoop-${ARTIFACT_VERSION}/etc/hadoop-templates
-cd ${INSTALL_DIR}
-find etc -type f -print | awk '{print "/" $1}' > /tmp/$$.files
-export CONFIG_FILES=""
-for i in `cat /tmp/$$.files`; do CONFIG_FILES="--config-files $i $CONFIG_FILES "; done
-export CONFIG_FILES
-rm -f /tmp/$$.files
-
-
-#interleave lzo jars
-for i in share/hadoop/httpfs/tomcat/webapps/webhdfs/WEB-INF/lib share/hadoop/mapreduce/lib share/hadoop/yarn/lib share/hadoop/common/lib; do
-  cp -rp ${WORKSPACE}/hadoop-lzo/target/hadoop-lzo-[0-9]*.[0-9]*.[0-9]*-[0-9]*[0-9].jar ${OPT_DIR}/hadoop-${ARTIFACT_VERSION}/$i
-done
-cp ${WORKSPACE}/hadoop-lzo/target/native/Linux-amd64-64/lib/libgplcompression.* ${OPT_DIR}/hadoop-${ARTIFACT_VERSION}/lib/native/
-
-cd ${RPM_DIR}
-
-export RPM_NAME=`echo vcc-hadoop-${ARTIFACT_VERSION}`
-fpm --verbose \
---maintainer ops@verticloud.com \
---vendor VertiCloud \
---provides ${RPM_NAME} \
---provides "libhdfs.so.0.0.0()(64bit)" \
---provides "libhdfs(x86-64)" \
---provides libhdfs \
---replaces vcc-hadoop \
---depends 'lzo > 2.0' \
--s dir \
--t rpm \
--n ${RPM_NAME}  \
--v ${ALTISCALE_RELEASE} \
---iteration ${DATE_STRING} \
---description "${DESCRIPTION}" \
-${CONFIG_FILES} \
---rpm-user root \
---rpm-group root \
--C ${INSTALL_DIR} \
-opt etc
